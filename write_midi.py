0e1c594bf2932732f2b02222ed1573bade981549
jinu99 <jinujinu99@gmail.com>
2020-06-15 13:47:43 +0900

    np_to_midi added

--- write_midi.py
+++ write_midi.py
@@ -4,18 +4,20 @@ import numpy as np
 
 def write_midi(pr, ticks_per_beat, write_path, tempo=80):
     def pr_to_list(pr):
+        print(pr.shape)
         # List event = (pitch, velocity, time)
-        T, N = pr.shape
+        data_num, max_len, N = pr.shape
+        T = data_num * max_len
         t_last = 0
         pr_tm1 = np.zeros(N)
         list_event = []
         for t in range(T):
-            pr_t = pr[t]
+            pr_t = pr[t // max_len][t % max_len]
             mask = (pr_t != pr_tm1)
             if (mask).any():
                 for n in range(N):
                     if mask[n]:
-                        pitch = n
+                        pitch = n + 19
                         velocity = int(pr_t[n])
                         # Time is incremented since last event
                         t_event = t - t_last
@@ -61,13 +63,24 @@ def write_midi(pr, ticks_per_beat, write_path, tempo=80):
             if velocity == 0:
                 # Get the channel
                 track.append(mido.Message('note_off', note=pitch, velocity=0, time=time))
-                notes_on_list.remove(pitch)
+                if pitch in notes_on_list:
+                    notes_on_list.remove(pitch)
             else:
                 if pitch in notes_on_list:
                     track.append(mido.Message('note_off', note=pitch, velocity=0, time=time))
-                    notes_on_list.remove(pitch)
+                    if pitch in notes_on_list:    
+                        notes_on_list.remove(pitch)
                     time = 0
                 track.append(mido.Message('note_on', note=pitch, velocity=velocity, time=time))
                 notes_on_list.append(pitch)
     mid.save(write_path)
     return
+
+def np_to_midi(name, tpb=30, tmp=80):
+    pr = np.load(name + ".npy", allow_pickle=True)
+    pr_dict = {"piano" : np.squeeze(pr)}
+
+
+    write_midi(pr_dict, tpb, name + ".mid", tempo=tmp)
+
+np_to_midi("beethoven64_test")
